af99d081588f97b547f0497bf79fee66654e909b
ΛLΣJΛNDRO ΛLFΛRO SΛNCHΣZ <67869168+alejandroalsa@users.noreply.github.com>
2023-02-16 17:05:28 +0100

    Update script_17.sh

--- script_17.sh
+++ script_17.sh
@@ -2,4 +2,20 @@
 clear
 
 #---->Explicación 56
+is_number() {
+  if echo "$1" | grep -Eq '^[-+]?[0-9]*\.?[0-9]+([eE][-+]?[0-9]+)?$'; then
+    return 0
+  else
+    return 1
+  fi
+}
 
+read -p "Introduce un valor numérico: " x
+
+#---->Explicación 57
+if is_number "$x"; then
+  resultado=$((3*x**2 + 5*x + 8))
+  echo "El resultado es: $resultado"
+else
+  echo "El valor introducido no es un número válido. Introduce un valor numérico."
+fi
